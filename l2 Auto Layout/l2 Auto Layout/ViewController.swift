cc0319827d0b81bf250c1ddbe736f757159afb61
James Bungay <jtb.github@gmail.com>
2020-07-07 14:29:05 +0100

    Complete lesson 2

---
+++ l2 Auto Layout/l2 Auto Layout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  l2 Auto Layout
+//
+//  Created by James Bungay on 07/07/2020.
+//  Copyright © 2020 James Bungay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
