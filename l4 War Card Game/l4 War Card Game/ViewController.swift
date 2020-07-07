11e3df606f61a234d27c11483b0cf491e25d3aee
James Bungay <jtb.github@gmail.com>
2020-07-07 16:34:27 +0100

    Complete lesson 4

---
+++ l4 War Card Game/l4 War Card Game/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  l4 War Card Game
+//
+//  Created by James Bungay on 07/07/2020.
+//  Copyright © 2020 James Bungay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
